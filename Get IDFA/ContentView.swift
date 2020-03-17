0e2311c2829b3979c1d4f5f726a45c53f9d0c4bf
banumelody <banumelody@gmail.com>
2020-03-17 10:45:36 +0700

    Initial Commit

---
+++ Get IDFA/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Get IDFA
+//
+//  Created by Banu Antoro on 17/03/20.
+//  Copyright © 2020 Banu Antoro. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
